e8ed15231e52173c2acf51c0f697a2f91597d689
Joa Ebert <joaebert@googlemail.com>
2012-08-07 12:53:42 +0200

    Undefine NULL in favor of nullptr

--- src/brutus.h
+++ src/brutus.h
@@ -1,6 +1,13 @@
 #ifndef _BRUTUS_H_
 #define _BRUTUS_H_
 
+// Undefine NULL in favor of nullptr
+#ifdef NULL
+  #undef NULL
+#endif
+
+// Macro to disallow the invocation of copy constructor
+// and assignment operator.
 #define DISALLOW_COPY_AND_ASSIGN(T) \
   T& operator=(const T&) = delete; \
   T(const T&) = delete;
